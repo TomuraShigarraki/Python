d58b5e555b2f69661d191e0cdfd81a8d5057ccc9
TomuraShigarraki <95834862+TomuraShigarraki@users.noreply.github.com>
2021-12-16 09:36:55 +0700

    Add files via upload

--- Chat_bot.py
+++ Chat_bot.py
@@ -1,11 +1,11 @@
-import pyautogui#
+import pyautogui,time#
 a=0
 b=0
 command=''
 while(command!='Стоп'):
     command=input('Введите команду')
     if command=='Помощь':
-            print('Вот список команд: Стоп, Сумма, Умножение, Деление, Разность, Быстрое закрытие')
+            print('Вот список команд: Стоп, Сумма, Умножение, Деление, Разность, Быстрое закрытие, Поиск')
     elif command=='Сумма':
             a=int(input('Введите первое число'))
             b=int(input('Введите второе число'))
@@ -28,6 +28,8 @@ while(command!='Стоп'):
             c=input('Введите запрос')
             pyautogui.moveTo(242,1044)
             pyautogui.click()
+            time.sleep(10)
             pyautogui.write(c)
+            time.sleep(10)
             pyautogui.press('enter')
-    
+    elif command==
