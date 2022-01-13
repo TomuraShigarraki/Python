23239654c49232e93afb1d97c6d34658aede2059
TomuraShigarraki <95834862+TomuraShigarraki@users.noreply.github.com>
2022-01-13 09:26:44 +0700

    Add files via upload

--- Chat_bot.py
+++ Chat_bot.py
@@ -4,29 +4,30 @@ import random
 a=0
 b=0
 command=''
-while(command!='Стоп'):
-    command=input('Введите команду')
-    if command=='Помощь':
-            print('Вот список команд: Стоп, Сумма, Умножение, Деление, Разность, Быстрое закрытие, Поиск')
-    elif command=='Сумма':
+print('Чтобы ознакомиться со списком комманд введите "помощь"')
+while(command!='стоп'):
+    command=input('Введите команду').lower()
+    if command=='помощь':
+            print('Вот список команд: Стоп, Сумма, Умножение, Деление, Разность, Быстрое закрытие, Поиск, Рандомайзер,Квадрат')
+    elif command=='сумма':
             a=int(input('Введите первое число'))
             b=int(input('Введите второе число'))
             print(a+b)
-    elif command=='Разность':
+    elif command=='разность':
             a=int(input('Введите первое число'))
             b=int(input('Введите второе число'))
             print(a-b)
-    elif command=='Умножение':
+    elif command=='умножение':
             a=int(input('Введите первое число'))
             b=int(input('Введите второе число'))
             print(a*b)
-    elif command=='Деление':
+    elif command=='деление':
             a=int(input('Введите первое число'))
             b=int(input('Введите второе число'))
             print(a/b)
-    elif command=='Быстрое закрытие':
+    elif command=='быстрое закрытие':
             pyautogui.hotkey('alt', 'f4')
-    elif command=='Поиск':
+    elif command=='поиск':
             c=input('Введите запрос')
             pyautogui.moveTo(242,1044)
             pyautogui.click()
@@ -34,7 +35,10 @@ while(command!='Стоп'):
             pyautogui.write(c)
             time.sleep(10)
             pyautogui.press('enter')
-    elif command=='Рандомайзер':
+    elif command=='рандомайзер':
             d=int(input('Введите масимальное число'))
             f=random.randint(0,d)
             print(f)
+    elif command=='квадрат':
+            for i in range(1,7):
+                print('******')
