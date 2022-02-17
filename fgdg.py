50728ba4b208fe2078b23153b4413f9bc47ced96
TomuraShigarraki <95834862+TomuraShigarraki@users.noreply.github.com>
2022-02-17 09:35:25 +0700

    Add files via upload

--- fgdg.py
+++ fgdg.py
@@ -1,20 +1,10 @@
 dela=['убраться','пропылесосить','покормить животных']
 command=''
-while command!='стоп':
+while(command!='стоп')
     command=input('Введите команду').lower()
-    if command=='добавить':
+    if command='добавить':
         a=input('Добавьте дело в список')
         dela.append(a)
-    elif command=='удалить':
+    elif command='удалить':
         b=input('Введите дело из списка для удаления')
         dela.remove(b)
-    elif command=='показать элемент по номеру':
-        c=int(input('Введите номер дела'))
-        print(dela[c])
-    elif command=='показать весь список':
-        print(dela)
-    elif command=='редактировать':
-        d=int(input('Введите номер дела'))
-        f=input('Введите на что хотите заменить')
-        dela.remove(dela[d])
-        dela.insert(d,f)
