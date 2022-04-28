61e3ad375e31c329a0b957503c53aa50ab861d75
TomuraShigarraki <95834862+TomuraShigarraki@users.noreply.github.com>
2022-04-28 09:39:30 +0700

    Add files via upload

---
+++ пппп.py
@@ -0,0 +1,10 @@
+import random
+def chislo():
+    a=random.randint(1,9)
+    b=random.randint(1,9)
+    c=random.randint(1,9)
+if a!=b and a!=c and b!=c:
+    d=int(input('Введите первую цифру'))
+    e=int(input('Введите вторую цифру'))
+    f=int(input('Введите третью цифру'))
+    def 
