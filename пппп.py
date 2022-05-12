39434d59243d8d7353fa0ba3c321c13c6fc757c2
TomuraShigarraki <95834862+TomuraShigarraki@users.noreply.github.com>
2022-05-12 10:41:06 +0700

    Add files via upload

--- пппп.py
+++ пппп.py
@@ -1,10 +1,19 @@
 import random
 def chislo():
     a=random.randint(1,9)
-    b=random.randint(1,9)
-    c=random.randint(1,9)
+    b=random.randint(0,9)
+    while b==a:
+        b=random.randint(0,9)
+    c=random.randint(0,9)
+    while c==a or c==b:
+        c=random.randint(0,9)
+    return str(a)+str(b)+str(c)
+def pobeda():
+    if t==h
+t=chislo()
+'''
 if a!=b and a!=c and b!=c:
     d=int(input('Введите первую цифру'))
     e=int(input('Введите вторую цифру'))
     f=int(input('Введите третью цифру'))
-    def 
+'''
